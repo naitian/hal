1f82f71e2cfe46d7fe14ef4e026547cee014fcb0
A. Shi <prounderboy@gmail.com>
2016-08-07 12:57:24 -0400

    working git integration

---
+++ commitstest.js
@@ -0,0 +1,5 @@
+var commits = require("./commits.js");
+
+commits.commitinfo('naitian', '2016-08-07T04:42:58Z', 'hal', '01758dd14cff8a74e2312d70a6b76e3f65f88b11', function(response){
+	console.log(response);
+});
