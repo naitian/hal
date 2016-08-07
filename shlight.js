dd19705084a251b392017c323b0793a51c6d712a
muakasan <muakasan@gmail.com>
2016-08-07 00:42:58 -0400

    syntax converted to module

--- shlight.js
+++ shlight.js
@@ -1,26 +1,26 @@
+"use strict";
+
 var hl = require("highlight").Highlight;
 var wkhtmltox = require("wkhtmltox");
 var fs = require("fs");
 var Readable = require("stream").Readable;
 
 var cssPath = __dirname+ "/node_modules/highlight/lib/vendor/highlight.js/styles/sunburst.css";
-var codeText =  "for(var i=0; i<10; i++)alert(i);"
-
-var html = hl(codeText);
-var htmlStng = "<link rel='stylesheet' href='" + cssPath+ "'/>" + "<pre>" + html + "</pre>";
 
 
-var converter = new wkhtmltox();
+module.exports = {
+    getSyntaxImage: function(codeText){
+        var html = hl(codeText);
+        var htmlStng = "<link rel='stylesheet' href='" + cssPath+ "'/>" + "<pre>" + html + "</pre>";
 
-converter.wkhtmltoimage = 'wkhtmltox/bin/wkhtmltoimage';
+        var converter = new wkhtmltox();
 
-var s = new Readable();
-s.push(htmlStng);
-s.push(null);
+        converter.wkhtmltoimage = 'wkhtmltox/bin/wkhtmltoimage';
 
+        var s = new Readable();
+        s.push(htmlStng);
+        s.push(null);
 
-converter.image(s, { format: "jpg" })
-    .pipe(fs.createWriteStream("foo.jpg"))
-    .on("finish", function(){
-        console.log("done");
-    });
+        return converter.image(s, { format: "jpg" });
+    }
+}
